7117b30b71c37c401026c04197d95921fe01b090
studentlaptop <159675326+studentlaptop@users.noreply.github.com>
2025-06-04 01:39:10 -0400

    Started implementing control event handler for GUI

--- FXTest/src/application/TextAreaExampleController.java
+++ FXTest/src/application/TextAreaExampleController.java
@@ -1,46 +1,41 @@
 package application;
-	
+
+import java.io.FileInputStream;
+import java.io.IOException;
+ 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-
-import entities.*;
-
-public class Main extends Application {
-
+ 
+public class TextAreaExampleController extends Application
+{
+    public static void main(String[] args) 
+    {
+        Application.launch(args);
+    }
+     
     @Override
-    public void start(Stage stage) throws IOException {
-    	// Create the FXMLLoader 
+    public void start(Stage stage) throws IOException 
+    {
+        // Create the FXMLLoader 
         FXMLLoader loader = new FXMLLoader();
         // Path to the FXML File
-        String fxmlDocPath = "TestAreaExample.fxml";
+        String fxmlDocPath = "TextAreaExampleController.fxml";
         FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
-         
+ 
         // Create the Pane and all Details
         AnchorPane root = (AnchorPane) loader.load(fxmlStream);
-         
+ 
         // Create the Scene
         Scene scene = new Scene(root);
         // Set the Scene to the Stage
         stage.setScene(scene);
         // Set the Title to the Stage
-        stage.setTitle("A SceneBuilder Example");
+        stage.setTitle("A SceneBuilder Example with a Controller");
         // Display the Stage
         stage.show();
     }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+ 
 }
